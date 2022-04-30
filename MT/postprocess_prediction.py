4e7c3a9ceb38264e25ab024b09d8ac2c6c78aa55
Ubuntu <ubuntu@ip-172-31-15-167.us-east-2.compute.internal>
2022-04-30 19:01:34 +0000

    modify postprocess script

--- MT/postprocess_prediction.py
+++ MT/postprocess_prediction.py
@@ -14,6 +14,7 @@ def get_args():
     parser.add_argument("--sent_ids", type=str, default=None)
     parser.add_argument("--final_pred", type=str)
     parser.add_argument("--gt_ids", type=str, default=None)
+    parser.add_argument("--sent_ids_prefix", type=str, default=None)
 
     args = parser.parse_args()
     return args
@@ -33,7 +34,7 @@ if __name__ == '__main__':
         gt_ids = f.read().split("\n")
 
 
-    if args.sent_ids and args.gt_ids:
+    if args.sent_ids:
         with open(args.sent_ids) as f:
             sent_ids = f.read().split("\n")
         assert len(raw_pred) == len(sent_ids)
@@ -44,7 +45,10 @@ if __name__ == '__main__':
 
     for sent_id, pred in zip(sent_ids, raw_pred):
         if sent_id:
-            final_dict[sent_id] += pred
+            if args.sent_ids_prefix is not None:
+                sent_id = args.sent_ids_prefix + f"{int(sent_id)+1:04d}"
+            if sent_id in final_dict:
+                final_dict[sent_id] += pred
 
 
     out_lines = []
