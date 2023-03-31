091ad3424c08f3bcca3653ebc43e0817e812456c
wchang88 <60525612+wchang88@users.noreply.github.com>
2023-03-31 16:25:47 -0400

    update translate script

--- MT/translate_nan_cmn.sh
+++ MT/translate_nan_cmn.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+RAW_DATA=data/suisiann_raw/nan_cmn/
 BINARIZED_DATA=data/suisiann_binarized/nan_spm8000/nan_cmn/
 MODEL_DIR=checkpoints/icorpus_nan_spm8000/nan_cmn/
 cp $BINARIZED_DATA/dict.nan.txt $BINARIZED_DATA/dict.cmn.txt
@@ -12,7 +13,13 @@ fairseq-generate $BINARIZED_DATA \
     --path $MODEL_DIR/checkpoint_best.pt \
     --batch-size 32 \
     --remove-bpe sentencepiece \
-    --beam 5 | grep ^H | cut -c 3- | sort -n | cut -f3- > "$MODEL_DIR"/suisiann_b5.pred
+    --beam 5 > "$MODEL_DIR"/suisiann_b5.rawpred
+
+echo "aligning fairseq-generate predictions to gold truths for suisiaan set"
+suisiann_gold_size=$(wc -l < "$RAW_DATA"/all.orig.nan)
+python postprocess_fairseq-gen.py "$MODEL_DIR"/suisiann_b5.rawpred \
+    "$MODEL_DIR"/suisiann_b5.pred \
+    "$suisiann_gold_size"
 
 
 ## TAT-Vol2
@@ -21,4 +28,10 @@ fairseq-generate $BINARIZED_DATA \
     --path $MODEL_DIR/checkpoint_best.pt \
     --batch-size 32 \
     --remove-bpe sentencepiece \
-    --beam 5 | grep ^H | cut -c 3- | sort -n | cut -f3- > "$MODEL_DIR"/tatvol2_b5.pred
+    --beam 5 > "$MODEL_DIR"/tatvol2_b5.rawpred
+
+echo "aligning fairseq-generate predictions to gold truths for tatvol2 set"
+tatvol2_gold_size=$(wc -l < "$RAW_DATA"/tatvol2.orig.nan)
+python postprocess_fairseq-gen.py "$MODEL_DIR"/tatvol2_b5.rawpred \
+    "$MODEL_DIR"/tatvol2_b5.pred \
+    "$tatvol2_gold_size"
